2d5f39d381e6c7a95f4a24716953e330626f0b62
lucerobarros <158118446+lucerobarros@users.noreply.github.com>
2024-02-20 22:41:29 -0300

    Add files via upload

---
+++ desafio_dragão_DIO.py
@@ -0,0 +1,14 @@
+c = int(input()) 
+for i in range (c):
+    n = int(input())
+    if 100 n <= 8000:
+        print('Inseto!')
+    
+    elif n > 8000:
+      print('Mais de 8000!')
+    
+   
+
+
+
+
